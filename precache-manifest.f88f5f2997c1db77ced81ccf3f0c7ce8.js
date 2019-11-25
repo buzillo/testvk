d66867d4edbdd6fbfa4a73cdce53c3439ea9ae20
VOLGINA TATYANA <volginatatyana@buzilko.local>
2019-11-25 19:18:08 +0300

    Updates

---
+++ precache-manifest.f88f5f2997c1db77ced81ccf3f0c7ce8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb754f69a07fbb36a941f61ea4de65bd",
+    "url": "/testvk/index.html"
+  },
+  {
+    "revision": "99c3819fc3994851ff4f",
+    "url": "/testvk/static/css/2.266626f9.chunk.css"
+  },
+  {
+    "revision": "2cc28cf2c99cccaa6561",
+    "url": "/testvk/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "99c3819fc3994851ff4f",
+    "url": "/testvk/static/js/2.d623afa2.chunk.js"
+  },
+  {
+    "revision": "2cc28cf2c99cccaa6561",
+    "url": "/testvk/static/js/main.43e3476a.chunk.js"
+  },
+  {
+    "revision": "2bc26de4867e6f80fd4e",
+    "url": "/testvk/static/js/runtime-main.7fff8040.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/testvk/static/media/persik.4e1ec840.png"
+  }
+]);
